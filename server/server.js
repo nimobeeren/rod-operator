7bfc05ef0a4dcf9c7ee7a67d3bf8fa3e24b77926
Nimo Beeren <nimobeeren@gmail.com>
2017-09-26 23:39:20 +0200

    Set up TCP client to talk to Arduino

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
+var net = require('net');
 var express = require('express');
 var server = express();
+var tcp = new net.Socket();
 
 server.use(express.static('client'));
 
@@ -7,6 +9,21 @@ server.get('/', function (req, res) {
     res.sendFile('/index.html');
 });
 
+server.get('/send', function (req, res) {
+    var msg = req.query['msg'];
+    console.log('Sending to Arduino: ' + msg);
+    tcp.write(msg);
+    res.send('Sent to Arduino: ' + msg);
+});
+
 server.listen(3000, function () {
     console.log('Server running');
+});
+
+tcp.connect(2001, '192.168.1.1', function () {
+    console.log('Connected to Arduino');
+});
+
+tcp.on('close', function () {
+    console.log('Connection to Arduino closed');
 });
