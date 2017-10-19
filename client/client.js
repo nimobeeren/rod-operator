0f70d417f322b29f823e94c780229bf314fd9542
Nimo Beeren <nimobeeren@gmail.com>
2017-10-19 23:20:23 +0200

    Continuously send gamepad state to Arduino

--- client/client.js
+++ client/client.js
@@ -1,40 +1,38 @@
 var px = new PxGamepad();
 px.start();
 
-px.on('leftTrigger', function () {
-    send('lt');
-});
-
-px.on('rightTrigger', function () {
-    send('lt');
-});
-
-px.on('b', function () {
-    send('b');
-});
-
-px.on('leftTop', function () {
-    send('lb');
-});
-
-px.on('rightTop', function () {
-    send('rb');
-});
-
-px.on('y', function () {
-    send('y');
-});
-
-px.on('x', function () {
-    send('x');
-});
-
-setInterval(function () {
-    px.update();
-    document.getElementById('buttons').innerHTML = JSON.stringify(px.buttons, null, 4);
-    document.getElementById('sticks').innerHTML = JSON.stringify(px.leftStick, null, 4) + JSON.stringify(px.rightStick, null, 4);
-}, 10);
-
 function send(msg) {
     jQuery.get('send', {msg: msg});
 }
+
+function encode(gamepad) {
+    // Encode button states into a 4 charachter hex string
+    var buttonsBinary = '';
+    for (var button in gamepad.buttons) {
+        if (!gamepad.buttons.hasOwnProperty(button)) continue;
+        buttonsBinary += gamepad.buttons[button] ? '1' : '0';
+    }
+    var buttonsHex = parseInt(buttonsBinary, 2).toString(16);
+    while (buttonsHex.length < 4) {
+        buttonsHex = '0' + buttonsHex;
+    }
+
+    return buttonsHex;
+}
+
+setInterval(function () {
+    // Update gamepad state
+    px.update();
+
+    if (px.getGamepad()) {
+        // Reflect gamepad state on UI, for debugging
+        document.getElementById('buttons').innerHTML = JSON.stringify(px.buttons, null, 4);
+        document.getElementById('sticks').innerHTML = JSON.stringify(px.leftStick, null, 4) + JSON.stringify(px.rightStick, null, 4);
+
+        // Send encoded gamepad state to Arduino
+        send(encode(px));
+    } else {
+        document.getElementById('buttons').innerHTML = 'No gamepad connected';
+        document.getElementById('sticks').innerHTML = '';
+    }
+}, 10);
