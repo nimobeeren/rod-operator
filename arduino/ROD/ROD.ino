06a02412ac740aedbb32bc963963a70fdd271ec7
Nimo Beeren <nimobeeren@gmail.com>
2017-10-22 01:28:02 +0200

    Abstract input button names and

--- arduino/ROD/ROD.ino
+++ arduino/ROD/ROD.ino
@@ -1,12 +1,26 @@
 #include <Servo.h>
 
+typedef union {
+    struct name {
+        bool a, b, x, y,
+            leftTop, rightTop, leftTrigger, rightTrigger,
+            select, start, leftStick, rightStick,
+            dpadUp, dpadDown, dpadLeft, dpadRight;
+    };
+    bool index[16];
+} DigitalButtons;
+
+typedef struct {
+    float x, y;
+} AnalogStick;
+
 // Controller state encoding
-int MESSAGE_SIZE = 25;
-int MESSAGE_BUTTONS[2] = {0, 4};
-int MESSAGE_LSX[2] = {5, 9};
-int MESSAGE_LSY[2] = {10, 14};
-int MESSAGE_RSX[2] = {15, 19};
-int MESSAGE_RSY[2] = {20, 24};
+const int PACKET_SIZE = 25;
+const int PACKET_BUTTONS[2] = {0, 4};
+const int PACKET_LSX[2] = {5, 9};
+const int PACKET_LSY[2] = {10, 14};
+const int PACKET_RSX[2] = {15, 19};
+const int PACKET_RSY[2] = {20, 24};
 
 // PWM out pins
 int pinWheelsLeft = 3;
@@ -18,9 +32,9 @@ int pinReverseLeft = 7;
 int pinReverseRight = 8;
 
 // Variables to hold controller state
-bool buttons[16];
-float leftStick[2];
-float rightStick[2];
+DigitalButtons buttons;
+AnalogStick leftStick;
+AnalogStick rightStick;
 float leftTrigger;
 float rightTrigger;
 
@@ -55,15 +69,15 @@ void setup() {
 }
 
 void loop() {
-	if (Serial1.available() >= MESSAGE_SIZE) {
+	if (Serial1.available() >= PACKET_SIZE) {
 	    // Print amount of received bytes
 	    Serial.print("Bytes: ");
 	    Serial.println(Serial1.available());
 
-		// Read the latest message
-		char message[MESSAGE_SIZE];
-		for (int i = 0; i < MESSAGE_SIZE; i++) {
-			message[i] = Serial1.read();
+		// Read the latest packet
+		char packet[PACKET_SIZE];
+		for (int i = 0; i < PACKET_SIZE; i++) {
+			packet[i] = Serial1.read();
 		}
 
 		// Discard the rest of the buffer
@@ -71,57 +85,57 @@ void loop() {
 			Serial1.read();
 		}
 
-		// Print received message
-		Serial.print("Message: ");
-		Serial.println(message);
+		// Print received packet
+		Serial.print("Packet: ");
+		Serial.println(packet);
 
 		// Decode controller state
-		decodeButtons(buttons, message);
-		decodeLeftStick(leftStick, message);
-		decodeRightStick(rightStick, message);
+		buttons = decodeButtons(packet);
+		leftStick = decodeLeftStick(packet);
+		rightStick = decodeRightStick(packet);
 
 		// Print button state
 		Serial.print("Buttons: ");
 		for (int i = 0; i < 16; i++) {
-			Serial.print(buttons[i]);
+			Serial.print(buttons.index[i]);
 		}
 		Serial.println();
 
 		// Print left stick state
 		Serial.print("Left stick: ");
-		Serial.print(leftStick[0]);
+		Serial.print(leftStick.x);
 		Serial.print(", ");
-		Serial.println(leftStick[1]);
+		Serial.println(leftStick.y);
 
 		// Print right stick state
         Serial.print("Right stick: ");
-        Serial.print(rightStick[0]);
+        Serial.print(rightStick.x);
         Serial.print(", ");
-        Serial.println(rightStick[1]);
+        Serial.println(rightStick.y);
 
 		// Wheels
-		if (buttons[7]) {
+		if (buttons.index[7]) {
 			// Move forwards
 			Serial.println("Moving forwards");
 			analogWrite(pinWheelsLeft, 255);
 			analogWrite(pinWheelsRight, 255);
 			digitalWrite(pinReverseLeft, LOW);
 			digitalWrite(pinReverseRight, LOW);
-		} else if (buttons[6]) {
+		} else if (buttons.index[6]) {
 			// Move backwards
 			Serial.println("Moving backwards");
 			analogWrite(pinWheelsLeft, 255);
 			analogWrite(pinWheelsRight, 255);
 			digitalWrite(pinReverseLeft, HIGH);
 			digitalWrite(pinReverseRight, HIGH);
-		} else if (buttons[4]) {
+		} else if (buttons.index[4]) {
 			// Turn left
 			Serial.println("Turning left");
 			analogWrite(pinWheelsLeft, 255);
 			analogWrite(pinWheelsRight, 255);
 			digitalWrite(pinReverseLeft, HIGH);
 			digitalWrite(pinReverseRight, LOW);
-		} else if (buttons[5]) {
+		} else if (buttons.index[5]) {
 			// Turn right
 			Serial.println("Turning right");
 			analogWrite(pinWheelsLeft, 255);
@@ -137,11 +151,11 @@ void loop() {
 			digitalWrite(pinReverseRight, LOW);
 		}
 
-		Serial.println();
+//		Serial.println();
 	}
 }
 
-void decodeButtons(bool *buf, char *msg) {
+DigitalButtons decodeButtons(char *packet) {
 	bool hash[16][4] = {
 		{0, 0, 0, 0}, {0, 0, 0, 1}, {0, 0, 1, 0}, {0, 0, 1, 1},
 		{0, 1, 0, 0}, {0, 1, 0, 1}, {0, 1, 1, 0}, {0, 1, 1, 1},
@@ -149,62 +163,75 @@ void decodeButtons(bool *buf, char *msg) {
 		{1, 1, 0, 0}, {1, 1, 0, 1}, {1, 1, 1, 0}, {1, 1, 1, 1}
 	};
 
-	// Get the button state from the message
-	int length = MESSAGE_BUTTONS[1] - MESSAGE_BUTTONS[0];
+	// Get button state from packet
+	int length = PACKET_BUTTONS[1] - PACKET_BUTTONS[0];
 	char buttonStr[length];
 	for (int i = 0; i < length; i++) {
-		buttonStr[i] = msg[MESSAGE_BUTTONS[0] + i];
+		buttonStr[i] = packet[PACKET_BUTTONS[0] + i];
 	}
 
 	// Convert hex string to bool array
+	DigitalButtons buttons;
 	for (int hexIndex = 0; hexIndex < length; hexIndex++) {
-		int location;
+	    // Convert hex character to decimal number
+		int decimal;
 		if (buttonStr[hexIndex] >= '0' && buttonStr[hexIndex] <= '9') {
-			location = buttonStr[hexIndex] - '0';
+			decimal = buttonStr[hexIndex] - '0';
 		} else if (buttonStr[hexIndex] >= 'a' && buttonStr[hexIndex] <= 'f') {
-			location = buttonStr[hexIndex] - 'a' + 10;
+			decimal = buttonStr[hexIndex] - 'a' + 10;
 		} else {
-			location = 0;
+			decimal = 0;
 		}
 
+        // Convert decimal number to 4-digit binary string using hash table
 		for (int binIndex = 0; binIndex < 4; binIndex++) {
-			buf[4 * hexIndex + binIndex] = hash[location][binIndex];
+			buttons.index[4 * hexIndex + binIndex] = hash[decimal][binIndex];
 		}
 	}
+	
+	return buttons;
 }
 
-void decodeLeftStick(float *buf, char *msg) {
-	// Get left stick X value from the message
-	int xLength = MESSAGE_LSX[1] - MESSAGE_LSX[0];
+AnalogStick decodeLeftStick(char *packet) {
+    AnalogStick stick;
+
+	// Get left stick X value from packet
+	int xLength = PACKET_LSX[1] - PACKET_LSX[0];
 	char xStr[xLength];
 	for (int i = 0; i < xLength; i++) {
-		xStr[i] = msg[MESSAGE_LSX[0] + i];
+		xStr[i] = packet[PACKET_LSX[0] + i];
 	}
-	buf[0] = atof(xStr) - 1.0;
+	stick.x = atof(xStr) - 1.0;
 
-	// Get left stick Y value from the message
-	int yLength = MESSAGE_LSY[1] - MESSAGE_LSY[0];
+	// Get left stick Y value from packet
+	int yLength = PACKET_LSY[1] - PACKET_LSY[0];
 	char yStr[yLength];
 	for (int i = 0; i < yLength; i++) {
-		yStr[i] = msg[MESSAGE_LSY[0] + i];
+		yStr[i] = packet[PACKET_LSY[0] + i];
 	}
-	buf[1] = atof(yStr) - 1.0;
+	stick.y = atof(yStr) - 1.0;
+
+	return stick;
 }
 
-void decodeRightStick(float *buf, char *msg) {
-	// Get right stick X value from the message
-	int xLength = MESSAGE_RSX[1] - MESSAGE_RSX[0];
+AnalogStick decodeRightStick(char *packet) {
+    AnalogStick stick;
+
+	// Get right stick X value from packet
+	int xLength = PACKET_RSX[1] - PACKET_RSX[0];
 	char xStr[xLength];
 	for (int i = 0; i < xLength; i++) {
-		xStr[i] = msg[MESSAGE_RSX[0] + i];
+		xStr[i] = packet[PACKET_RSX[0] + i];
 	}
-	buf[0] = atof(xStr) - 1.0;
+	stick.x = atof(xStr) - 1.0;
 
-	// Get right stick Y value from the message
-	int yLength = MESSAGE_RSY[1] - MESSAGE_RSY[0];
+	// Get right stick Y value from packet
+	int yLength = PACKET_RSY[1] - PACKET_RSY[0];
 	char yStr[yLength];
 	for (int i = 0; i < yLength; i++) {
-		yStr[i] = msg[MESSAGE_RSY[0] + i];
+		yStr[i] = packet[PACKET_RSY[0] + i];
 	}
-	buf[1] = atof(yStr) - 1.0;
+	stick.y = atof(yStr) - 1.0;
+
+	return stick;
 }
